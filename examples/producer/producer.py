141b873b519a0b4d6b1c6d001dcc69a367021a83
Luc <luc.juggery@gmail.com>
2024-01-30 17:56:07 +0100

    fix out of range issue (#6)
    * cleanup producer a bit
    
    * change screenshots

--- examples/producer/producer.py
+++ examples/producer/producer.py
@@ -1,5 +1,4 @@
 from kafka import KafkaProducer
-from faker import Faker
 import pycountry
 import random
 import time
@@ -9,26 +8,18 @@ bootstrap_servers = f"{os.environ['KAFKA_HOST']}:{os.environ['KAFKA_PORT']}"
 topic = os.environ['KAFKA_TOPIC']
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-# Set up Faker to generate country-related data
-fake = Faker()
-
+# Get total number of countries
 total_countries = len(pycountry.countries)
 
 # Generate a dummy messsage every second
-while True:
-    # Get random country code and related name
-    country_code = fake.country_code()
-    country_description = fake.country()
-    
-    # Convert to bytes
-    key = country_code.encode('utf-8')
-    value = country_description.encode('utf-8')
-        
-    random_index = random.randint(0, total_countries) 
+while True:        
+    # Get a random country
+    random_index = random.randint(0, total_countries-1) 
     country = list(pycountry.countries)[random_index]
-    print(country)
 
-    value = "alpha_2: {}, alpha_3: {}, name: {}, numeric: {}".format(country.alpha_2, country.alpha_3, country.name, country.numeric).encode('utf-8')
+    # Define key and value
+    key = country.alpha_2.encode('utf-8')
+    value = "alpha_3: {}, name: {}, numeric: {}".format(country.alpha_3, country.name, country.numeric).encode('utf-8')
 
     # Sent message
     try:
